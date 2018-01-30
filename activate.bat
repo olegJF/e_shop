1b110dfc8e8a47466d250f72f372c62440e29b70
Oleg Novikov <jf2@ua.fm>
2018-01-30 16:20:36 +0200

    after merge

--- activate.bat
+++
@@ -1,1 +0,0 @@
-"C:\Users\Oleg\shop\Scripts\activate.bat"
--- e_shop/settings/__init__.py
+++ e_shop/settings/__init__.py
@@ -1,13 +1,8 @@
 from .base import *
 
+from .production import *
 
 try:
 	from .local import *
-except:
-	pass
-
-	
-try:
-	from .production import *
 except:
 	pass
